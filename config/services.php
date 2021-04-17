cf8a89a66182ce870eed90dd53f562c7822011d5
pfazzi <patrick91@live.it>
2021-04-18 00:12:58 +0200

    Services config is now based on DSL

---
+++ config/services.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+
+use BookShop\Application\Query;
+use BookShop\Kernel;
+use ReflectionClass;
+
+use function array_map;
+
+$classToFileName    = static fn (string $fqcn): string => (new ReflectionClass($fqcn))->getFileName();
+$classesToFileNames = static fn (string ...$fqcns): array => array_map($classToFileName, $fqcns);
+
+return static function (ContainerConfigurator $configurator) use ($classToFileName, $classesToFileNames): void {
+    $services = $configurator->services()
+        ->defaults()
+            ->autowire()
+            ->autoconfigure();
+
+    $services->load(namespace: 'BookShop\\', resource: '../src/*')
+        ->exclude([
+            '../src/Application/Command/',
+            '../src/Application/Query/',
+            '../src/Domain/',
+            $classToFileName(Kernel::class),
+        ]);
+
+    $services->load(
+        namespace: 'BookShop\Ports\ShopApi\Controller\\',
+        resource: '../src/Ports/ShopApi/Controller/'
+    )->tag('controller.service_arguments');
+
+    $services->load(
+        namespace: 'BookShop\Ports\BackOfficeApi\Controller\\',
+        resource: '../src/Ports/BackOfficeApi/Controller/'
+    )->tag('controller.service_arguments');
+
+    $services->load(
+        namespace: 'BookShop\Application\Command\\',
+        resource: '../src/Application/Command/**/**Handler.php'
+    )->tag('messenger.message_handler');
+
+    $services->load(
+        namespace: 'BookShop\Application\Query\\',
+        resource: '../src/Application/Query/'
+    )->exclude($classesToFileNames(
+        Query\BackOffice\Author\Author::class,
+        Query\BackOffice\Book\Book::class,
+        Query\Shop\Book\Author::class,
+        Query\Shop\Book\Book::class,
+    ));
+};
